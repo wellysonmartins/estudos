abf3eebc5341ae62d02b4d07a7722a47198a431b
Wellyson Martins <wellysonmartins@gmail.com>
2021-05-13 10:07:48 -0300

    Lifecycle methods - react component

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "Título 1",
+        body: "Corpo 1",
+      },
+      {
+        id: 2,
+        title: "Título 2",
+        body: "Corpo 2",
+      },
+      {
+        id: 3,
+        title: "Título 3",
+        body: "Corpo 3",
+      },
+    ],
+  };
+  timeoutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "O título mudou";
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 3000);
+  };
+
+  render() {
+    const { posts, counter } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
